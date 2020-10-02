2a6fe5f42e8ffd7f602f96b744b1ef4fcc6715ad
jmilll <jmillgames@gmail.com>
2020-10-02 16:48:00 -0400

    .

--- script.js
+++ script.js
@@ -2,16 +2,12 @@ const playButton = document.getElementById('play-button');
 const container = document.getElementById('container');
 const playerContainer = document.getElementById('player-selection');
 const userfighter = document.getElementById('user-fighter');
-const computerContainer = document.getElementById('computer-selection');
 const computerfighter = document.getElementById('computer-fighter');
 const starterDiv = document.getElementById('starter');
-
 const scoreboard = document.getElementById('score');
 const userBalls = document.querySelectorAll('.ball.user-ball');
 const cpuBalls = document.querySelectorAll('.ball.cpu-ball');
-
 const roundResult = document.getElementById('round-result');
-
 const activeButton = document.querySelectorAll('.active-button');
 
 const winningScore = 5;
@@ -49,7 +45,6 @@ function round(playerSelection, computerSelection) {
         roundScore++;
         userBalls[(playerScore - 1)].setAttribute('class', 'ball user-ball win-round');
         return(`It was SUPER EFFECTIVE, ${playerSelection} beats ${computerSelection}!`);
-        
   } else {
     compScore++;
     roundScore++;
@@ -78,10 +73,8 @@ function knockOut() {
     newDiv.appendChild(newSad);
     newDiv.appendChild(newButton);
     newDiv.appendChild(newText);
-
-
-    //againButton.addEventListener('click', () => {starterDiv.remove()});
 }
+
 // same as knockout, but different results for winning
 function winner() {
     const newDiv = document.createElement('div');
@@ -94,7 +87,6 @@ function winner() {
 
     const newText = document.createElement('p');
     newText.setAttribute('class', 'post-game');
-    //change what it says when win
     newText.textContent = 'Hey, you won! You gained 12,000EXP points! You got $1500, sent some home to mom.';
 
     const newTrophy = document.createElement('div');
@@ -104,10 +96,8 @@ function winner() {
     newDiv.appendChild(newTrophy);
     newDiv.appendChild(newButton);
     newDiv.appendChild(newText);
-
-
-    //againButton.addEventListener('click', () => {starterDiv.remove()});
 }
+
 function rematch() {
     const x = document.getElementById('play-again');
     const y = document.getElementById('ko');
@@ -119,7 +109,6 @@ function rematch() {
         computerfighter.setAttribute('class', '');
         roundResult.textContent = 'COMPUTER wants to fight! Select an attack.';
     });
-
 }
 
 function checkGameOver() {    
@@ -127,37 +116,14 @@ function checkGameOver() {
         roundResult.textContent = 'The match is yours';
         winner();
         rematch();
-        //activeButton.forEach(activeButton => activeButton.classList.remove('active-button'));
     } else if (compScore === winningScore) {
         roundResult.textContent = 'You\'ve been defeated, you white out';
         knockOut();
         rematch();
-        //activeButton.forEach(activeButton => activeButton.classList.remove('active-button'));
     } else {
         console.log('');
     }
 }
-
-
-/*
-//to take functions off buttons and call instead
-function testRound() {
-    let playerSelection = this.textContent;
-    let computerSelection = computerPlay();
-    roundResult.textContent = (round(playerSelection, computerSelection));
-    console.log(`Score: ${playerScore} to ${compScore} // Round: ${roundScore}`);
-    scoreboard.textContent = (`Score: ${playerScore} to ${compScore} // Round: ${roundScore}`);
-
-    checkGameOver();
-}
-*/
-/*
-        let playerSelection = 'rock';
-        let computerSelection = computerPlay();
-        console.log(round(playerSelection, computerSelection));
-        console.log(`Score: ${playerScore} to ${compScore} // Round: ${roundScore}`);
-*/
-/*     */
 
 activeButton.forEach(activeButton => activeButton.addEventListener('click', (e) => {
     if (button = e.target.matches("#rock")) {
@@ -197,44 +163,4 @@ activeButton.forEach(activeButton => activeButton.addEventListener('click', (e) 
     }
 }));
 
-
 playButton.addEventListener('click', () => {starterDiv.remove()});
-
-
-//this.arrowsHolder.forEach(arrow => arrow.addEventListener('click', e => console.log(arrow.id)));
-
-
-
-
-
-/**
- * Play a single round of the game, updating the round results as necessary.
- */
-function playRound() {
-    // 'this' refers to button element calling this function.
-    const playerSelection = this.textContent.toLowerCase();
-    const computerSelection = computerPlay().toLowerCase();
-    
-    // Element (containing each player's action) class lists
-    const classListPlayer = document.querySelector('.player-action').classList;
-    const classListComp = document.querySelector('.computer-action').classList;
-    let winner;
-
-    updateActionImage(classListPlayer, playerSelection);
-    updateActionImage(classListComp, computerSelection);
-
-    // Tie between player and computer.
-    if (playerSelection === computerSelection) {
-        return;
-    } else {
-        winner = findWinner(playerSelection, computerSelection);
-        updateScore(winner);
-    }
-
-    checkGameOver();
-}
-//const BUTTONS = [...document.querySelectorAll('.player .action-button')];
-// Add event listener to each button.
-//for (let i = 0; i < BUTTONS.length; i++) {
-//    BUTTONS[i].addEventListener('click', playRound);
-//}
